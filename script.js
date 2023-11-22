43afca39edb9c7b89b2cd6ed55a9d798b981f037
MauricioCarreon7 <90652249+MauricioCarreon7@users.noreply.github.com>
2023-11-22 10:11:51 -0600

    project

---
+++ script.js
@@ -0,0 +1,75 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+function sendMessage() {
+
+  alert("Message Send");
+}
+
+// Dark / light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+// const iconFont = document.getElementById("font");
+
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+btn.addEventListener("click", function () {
+  setTheme();
+});
+
+btn2.addEventListener("click", function () {
+  setTheme();
+});
+
+function setTheme() {
+  let currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+
+  const icons = document.querySelectorAll(".icon-font");
+  icons.forEach((icon) => {
+    icon.style.color = "white";
+  });
+  
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
+
+  const icons = document.querySelectorAll(".icon-font");
+  icons.forEach((icon) => {
+    icon.style.color = "black";
+  });
+  
+}
+
+
+
+
